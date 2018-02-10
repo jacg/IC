02dc48ad3678aabca22ed42b7e3eebe9d88fc3a4
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2018-02-10 15:40:36 +0100

    Correct misleading type annotation in hist_writer

--- invisible_cities/io/hist_io.py
+++ invisible_cities/io/hist_io.py
@@ -23,7 +23,7 @@ def hist_writer(file,
     ## The bins can be written just once at definition of the writer
     file.create_array(hist_group, table_name+'_bins', bin_centres)
     
-    def write_hist(waveform : 'np.array: RWF, CWF, SiPM'):
+    def write_hist(waveform : 'np.array of histograms, one for each sensor'):
         hist_table.append(waveform.reshape(1, n_sensors, n_bins))
         
     return write_hist
